59e0aced1f95e72472da1fad08087d51aaf61dd3
TheOGChips <44422266+TheOGChips@users.noreply.github.com>
2021-06-13 15:34:43 -0500

    Made Wi-Fi username and password generic.

--- eth_static_ip_setup.sh
+++ eth_static_ip_setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 ###### IMPORTANT: Run this script as root! It will not work properly if you don't!!! ######
 
-# just do this through raspi-config
-#hostname clusterphuq"$0" # number of the cluster node
+# just do this via raspi-config
+#hostname clusterphuq"$1" # number of the cluster node
 
 # configure static Ethernet IP address
 filepath=/etc/dhcpcd.conf
 echo "interface eth0" >> "$filepath"
 echo "static ip_address=10.10.10.$1/16" >> "$filepath"	# $1 should be the same as the # of the node
 
-# configure Wi-Fi connection
-#filepath=/etc/wpa_supplicant/wpa_supplicant.conf	#TODO: Check if first 3 lines are already in file
+# configure Wi-Fi connection (alternatively, can also be done via raspi-config)
+#filepath=/etc/wpa_supplicant/wpa_supplicant.conf
 #echo "network={" >> "$filepath"
-#echo "        ssid=\"Eagle Summit 5g\"" >> "$filepath"
-#echo "        psk=\"Tenant@ES\!\"" >> "$filepath"
+#echo "        ssid=\"<put Wi-Fi SSID here>\"" >> "$filepath"
+#echo "        psk=\"<put Wi-Fi password here>\"" >> "$filepath"
 #echo "}" >> "$filepath"
